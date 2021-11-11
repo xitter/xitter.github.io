58413f4f16bcba3fb829db8bbc9516854712469f
Vijay Jain <vijayjain@google.com>
2021-11-11 16:49:08 +0530

    a

--- js/scripts.js
+++ js/scripts.js
@@ -85,37 +85,37 @@ $(document).ready(function () {
 
     // /***************** Header BG Scroll ******************/
 
-    // $(function () {
-    //     $(window).scroll(function () {
-    //         var scroll = $(window).scrollTop();
+    $(function () {
+        $(window).scroll(function () {
+            var scroll = $(window).scrollTop();
 
-    //         // if (scroll >= 0) {
-    //         //     $('section.navigation').addClass('fixed');
-    //         //     $('header').css({
-    //         //         "border-bottom": "none",
-    //         //         "padding": "35px 0"
-    //         //     });
-    //         //     $('header .member-actions').css({
-    //         //         "top": "26px",
-    //         //     });
-    //         //     $('header .navicon').css({
-    //         //         "top": "34px",
-    //         //     });
-    //         // } else {
-    //         //     $('section.navigation').removeClass('fixed');
-    //         //     $('header').css({
-    //         //         "border-bottom": "solid 1px rgba(255, 255, 255, 0.2)",
-    //         //         "padding": "50px 0"
-    //         //     });
-    //         //     $('header .member-actions').css({
-    //         //         "top": "41px",
-    //         //     });
-    //         //     $('header .navicon').css({
-    //         //         "top": "48px",
-    //         //     });
-    //         // }
-    //     });
-    // });
+            if (scroll >= 0) {
+                $('section.navigation').addClass('fixed');
+                $('header').css({
+                    "border-bottom": "none",
+                    "padding": "35px 0"
+                });
+                $('header .member-actions').css({
+                    "top": "26px",
+                });
+                $('header .navicon').css({
+                    "top": "34px",
+                });
+            } else {
+                $('section.navigation').removeClass('fixed');
+                $('header').css({
+                    "border-bottom": "solid 1px rgba(255, 255, 255, 0.2)",
+                    "padding": "50px 0"
+                });
+                $('header .member-actions').css({
+                    "top": "41px",
+                });
+                $('header .navicon').css({
+                    "top": "48px",
+                });
+            }
+        });
+    });
     // /***************** Smooth Scrolling ******************/
 
     $(function () {
